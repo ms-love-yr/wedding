06c757225aff0af5d50cfb1e024f7c2d81112bf0
MyeongSuk Yoon <yoonms0101@gmail.com>
2026-04-08 20:49:28 +0900

    1차 수정

--- site-data.js
+++ site-data.js
@@ -1,32 +1,132 @@
 window.SITE_DATA = {
   "gallery": [
     {
-      "src": "images/gallery/4M7A0395-1.jpg",
-      "title": "4M7A0395 1"
+      "src": "images/gallery/01.jpg",
+      "title": "01"
     },
     {
-      "src": "images/gallery/4M7A0662-1.jpg",
-      "title": "4M7A0662 1"
+      "src": "images/gallery/02.jpg",
+      "title": "02"
     },
     {
-      "src": "images/gallery/4M7A2137-1.jpg",
-      "title": "4M7A2137 1"
+      "src": "images/gallery/03.jpeg",
+      "title": "03"
     },
     {
-      "src": "images/gallery/4M7A6291-1.jpg",
-      "title": "4M7A6291 1"
+      "src": "images/gallery/04.jpeg",
+      "title": "04"
     },
     {
-      "src": "images/gallery/4M7A6991-1.jpg",
-      "title": "4M7A6991 1"
+      "src": "images/gallery/05.jpeg",
+      "title": "05"
     },
     {
-      "src": "images/gallery/4M7A8398-1.jpg",
-      "title": "4M7A8398 1"
+      "src": "images/gallery/06.jpeg",
+      "title": "06"
     },
     {
-      "src": "images/gallery/4M7A8844-1.jpg",
-      "title": "4M7A8844 1"
+      "src": "images/gallery/07.jpeg",
+      "title": "07"
+    },
+    {
+      "src": "images/gallery/08.jpeg",
+      "title": "08"
+    },
+    {
+      "src": "images/gallery/09.jpeg",
+      "title": "09"
+    },
+    {
+      "src": "images/gallery/10.jpeg",
+      "title": "10"
+    },
+    {
+      "src": "images/gallery/11.jpeg",
+      "title": "11"
+    },
+    {
+      "src": "images/gallery/12.jpeg",
+      "title": "12"
+    },
+    {
+      "src": "images/gallery/13.jpeg",
+      "title": "13"
+    },
+    {
+      "src": "images/gallery/14.jpeg",
+      "title": "14"
+    },
+    {
+      "src": "images/gallery/15.jpeg",
+      "title": "15"
+    },
+    {
+      "src": "images/gallery/16.jpeg",
+      "title": "16"
+    },
+    {
+      "src": "images/gallery/17.jpeg",
+      "title": "17"
+    },
+    {
+      "src": "images/gallery/18.jpeg",
+      "title": "18"
+    },
+    {
+      "src": "images/gallery/19.jpeg",
+      "title": "19"
+    },
+    {
+      "src": "images/gallery/20.jpeg",
+      "title": "20"
+    },
+    {
+      "src": "images/gallery/21.jpeg",
+      "title": "21"
+    },
+    {
+      "src": "images/gallery/22.jpeg",
+      "title": "22"
+    },
+    {
+      "src": "images/gallery/23.jpeg",
+      "title": "23"
+    },
+    {
+      "src": "images/gallery/24.jpeg",
+      "title": "24"
+    },
+    {
+      "src": "images/gallery/25.jpeg",
+      "title": "25"
+    },
+    {
+      "src": "images/gallery/26.jpeg",
+      "title": "26"
+    },
+    {
+      "src": "images/gallery/27.jpeg",
+      "title": "27"
+    },
+    {
+      "src": "images/gallery/28.jpeg",
+      "title": "28"
+    },
+    {
+      "src": "images/gallery/29.jpeg",
+      "title": "29"
+    },
+    {
+      "src": "images/gallery/30.jpeg",
+      "title": "30"
+    },
+    {
+      "src": "images/gallery/31.jpeg",
+      "title": "31"
+    },
+    {
+      "src": "images/gallery/32.jpeg",
+      "title": "32"
     }
   ],
   "past": [
